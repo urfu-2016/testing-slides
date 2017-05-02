b9a9f69ddd73795fbedb8b235583a77e84e16225
Zhigalov <sergey.zhigalov@gmail.com>
2017-05-02 14:37:37 +0500

    PhantomJS

--- 07-integration/public/tests/convertor-test.js
+++ 07-integration/public/tests/convertor-test.js
@@ -1,11 +1,11 @@
-describe('Convertor controls', () => {
-    it('should enable `convert` button for correct value', () => {
+describe('Convertor controls', function() {
+    it('should enable `convert` button for correct value', function() {
         const send = document.getElementById('send');
 
         chai.assert.equal(send.getAttribute('disabled'), null);
     });
 
-    it('should disable `convert` button for invalid value', () => {
+    it('should disable `convert` button for invalid value', function() {
         const from = document.getElementById('from');
         const send = document.getElementById('send');
 
