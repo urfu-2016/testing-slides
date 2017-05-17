ba985a5237fd61e7e1ab4854c206016a36a1b858
Zhigalov <sergey.zhigalov@gmail.com>
2017-05-17 16:33:15 +0500

    Правки после прогона

--- 09-tools/eslintWrongExamples/codestyle.js
+++ 09-tools/eslintWrongExamples/codestyle.js
@@ -1,8 +1,9 @@
-/* eslint no-unused-vars: 0 */
-
-let firstName = 'Sergey',
-    secondName;
-const params = {
-    language: 'JavaScript', testable: true,
-    version: 'ES2016'
-};
+// function formatDate(date) {
+//     const hours = (`0${date.getHours()}`).slice(-2);
+//     const minutes = (`0${date.getMinutes()}`).slice(-2);
+//     const nextDay = new Date(date.setDate(date.getDate() + 1)).toDateString();
+//
+//     if (nextDay == new Date()) { return `в ${hours}:${minutes}`; }
+//
+//     /* ... */
+// }
